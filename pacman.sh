bd807ad0504fd3964c9bc77073a8a9fc2dd0258c
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-02-22 10:50:40 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -95,12 +95,13 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 else
     echo ">> Installing Oh my ZShell..."
     yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="jonathan"/' ~/.zshrc
-    source ~/.zshrc
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/binding "'<Super>t'"
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/command "'gnome-terminal -- zsh'"
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/name "'terminal'"
 fi
+echo ">> Changing ZSH Theme!"
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="agnoster"/' ~/.zshrc
+source ~/.zshrc
 #################### PACKAGES ####################
 
 
